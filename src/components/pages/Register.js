59b0e7b4cfb7b6972f5acd59253a793e1cf30cd4
MoreiraSte <casagrandestephany91@gmail.com>
2022-09-28 11:35:31 -0300

    page download finish, add inputs in sign up

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -6,16 +6,24 @@ import { Link } from 'react-router-dom';
 export default function Register() {
   return(
     <div className='sign-up'>
-    <div className='container'>
+    <div className='containerRes'>
       <h4>Sign Up</h4>
       <div className='info'>
       <input type="text" id="name" name="Nome" placeholder='Nome'/>
       <br></br>
       <input type="number" id="age" name="Idade" placeholder='Idade'/>
       <br></br>
+      <input type="date" id="nasc" name="Nasc"/>
+      <br></br>
       <input type="text" id="phone" name="Celular" placeholder='Celular'/>
       <br></br>
-      <input type="date" id="nasc" name="Nasc"/>
+      <input type="email" id="email" name="Email" placeholder='E-mail'/>
+      <br></br>
+      <input type="text" id="cpf" name="Cpf" placeholder='CPF'/>
+      <br></br>
+      <input type="password" id="senha" name="Senha" placeholder='Senha'/>
+      <br></br>
+      <input type="password2" id="senha2" name="Senha2" placeholder='Repita a senha'/>
       <br></br>
       <div className='selection'>
         <div className='genero'>
@@ -41,9 +49,16 @@ export default function Register() {
         </Link>
     
       <div className='link'>
+        <div>
       <Link to='/sign-up' className='signup' >
                 Sign In
         </Link>
+        </div>
+        <div>
+      <Link to='/' className='signup' >
+                Forget the password? Click here 
+        </Link>
+        </div>
       </div>
     </div>
   </div>
