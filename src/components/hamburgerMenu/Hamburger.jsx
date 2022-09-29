a3adc2523fa9b19563a43b550377703e197942c7
MoreiraSte <casagrandestephany91@gmail.com>
2022-09-29 17:00:46 -0300

    responsividade tela services

--- src/components/hamburgerMenu/Hamburger.jsx
+++ src/components/hamburgerMenu/Hamburger.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu'
 import './style.css'
+import { Link } from 'react-router-dom';
+
 
 function Hamburger (){
     return(
         <Menu right >
-        <a id="home" className="menu-item" href="/">Home</a>
-        <a id="about" className="menu-item" href="/about">About</a>
-        <a id="contact" className="menu-item" href="/contact">Contact</a>
-        <p>tydoysayd\sy</p>
+          <Link to='/' className='menu-item'>
+                Home
+              </Link>
+              <Link
+                to='/about'
+                className='menu-item'
+              >
+                About
+              </Link>
+              <Link
+                to='/services'
+                className='menu-item'
+             
+              >
+                Services
+              </Link>
+              <Link
+                to='/download'
+                className='menu-item'
+              
+              >
+                Download app
+              </Link>
+              <Link
+                to='/sign-up'
+                className='menu-item'
+              
+              >
+                Sign In
+              </Link>
+        
       </Menu>
     )
 }
