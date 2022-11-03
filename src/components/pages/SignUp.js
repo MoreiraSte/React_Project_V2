129a20de60c9bd6e03081ac73e284b6663d8debe
MoreiraSte <casagrandestephany91@gmail.com>
2022-11-03 11:17:45 -0300

    authenticated login - part 1

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -6,30 +6,30 @@ import { Link, useHistory } from 'react-router-dom';
 export default function SignUp() {
 
   
-  // const [email,setEmail]= useState("");
-  // const [password,setPassword] = useState("");
-  // const history = useHistory();
-  // useEffect(() => {
-  //     if(localStorage.getItem('user-info')){
-  //       history.push('/add')
-  //     }
-  // },[])
+  const [email,setEmail]= useState("");
+  const [password,setPassword] = useState("");
+  const history = useHistory();
+  useEffect(() => {
+      if(localStorage.getItem('user-info')){
+        history.push('/add')
+      }
+  },[])
 
-  // const loginSign = () => {   
-  //   console.log.warn(email,password)
-  //   let item = {email,password}
-  //   let result= fetch("localhost:8000/user",{
-  //     method:'POST',
-  //     body:JSON.stringify(item),
-  //     headers:{
-  //       "Cotent-Type":'application/json',
-  //       "Accept":'application/json'
-  //     }
-  //   })
-  //   result = result.json()
-  //   localStorage.setItem("user-info",JSON.stringify(result))
-  //   history.push("/add")
-  // }
+  const loginSign = () => {   
+    console.log.warn(email,password)
+    let item = {email,password}
+    let result= fetch("localhost:8000/user",{
+      method:'POST',
+      body:JSON.stringify(item),
+      headers:{
+        "Cotent-Type":'application/json',
+        "Accept":'application/json'
+      }
+    })
+    result = result.json()
+    localStorage.setItem("user-info",JSON.stringify(result))
+    history.push("/add")
+  }
 
   return(
 
@@ -37,17 +37,14 @@ export default function SignUp() {
     <div className='container'>
       <h4>Sign In</h4>
       <div className='info'>
-      <input type="text"  id="email" name="email" placeholder='email'/>
-      {/* value={email} onChange={(e)=>setEmail(e.target.value)} */}
+      <input type="text"   value={email} onChange={(e)=>setEmail(e.target.value)} id="email" name="email" placeholder='email'/>
+     
       <br></br>
-      <input type="text"  id="senha" name="senha" placeholder='senha'/>
-      {/* value={password} onChange={(e)=>setPassword(e.target.value)} */}
+      <input type="text"  value={password} onChange={(e)=>setPassword(e.target.value)} id="senha" name="senha" placeholder='senha'/>
+      
       </div>
       
-      <Link to='/' className='button'  > 
-      {/* onClick={loginSign} */}
-                Logar
-        </Link>
+      <button className='button' onClick={loginSign} >Submit</button>
     
       <div className='link'>
       <Link to='/register' className='signup' >
